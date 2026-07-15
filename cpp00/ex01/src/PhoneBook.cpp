b15bb04ec9d0e246ca1a1413f37c6ab3715713f0
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-07-15 12:41:16 +0300

    implement takeField method and enhance addContact functionality in PhoneBook class

--- cpp00/ex01/src/PhoneBook.cpp
+++ cpp00/ex01/src/PhoneBook.cpp
@@ -1,5 +1,9 @@
 #include "PhoneBook.hpp"
 
+PhoneBook::PhoneBook() : totalContacts(0) {} //int bir ilkel (primitive) tiptir, sınıf değildir. Primitive tiplerin "default constructor"ı diye bir şey yoktur
+// eğer sen elle bir değer vermezsen, çöp (garbage) değer kalır, yani bellekte o an ne varsa o kalır
+PhoneBook::~PhoneBook() {}
+
 void PhoneBook::setTotalContacts(int totalContacts)
 {
     this->totalContacts = totalContacts;
@@ -10,9 +14,81 @@ int PhoneBook::getTotalContacts() const
     return totalContacts;
 }
 
+int PhoneBook::takeField(std::string *field, const std::string output)
+{
+    while (1)
+    {
+        std::cout << "Enter a " << output;
+        std::cin >> *field;
+        if (std::cin.eof())
+            return (1);
+        if (field->empty())
+            std::cout << "Enter a " << output;
+        else
+            return (0);
+    }
+}
+
 void PhoneBook::addContact()
 {
+    int count = 0;
+    std::string firstName;
+    std::string lastName;
+    std::string nickName;
+    std::string phoneNumber;
+    std::string darkestSecret;
 
+    while (count < 5)
+    {
+        if (count == 0)
+        {
+            if(takeField(&firstName, "first name:"))
+                break;
+            else
+                count++;
+        }
+        else if (count == 1)
+        {
+            if(takeField(&lastName, "last name:"))
+                break;
+            else
+                count++;
+        }
+        else if (count == 2)
+        {
+            if(takeField(&nickName, "nick name:"))
+                break;
+            else
+                count++;
+        }
+        else if (count == 3)
+        {
+            if(takeField(&phoneNumber, "phone number:"))
+                break;
+            else
+                count++;
+        }
+        else if (count  == 4)
+        {
+            if(takeField(&darkestSecret, "secret:"))
+                break;
+            else
+                count++;
+        }
+        else
+            break;
+    }
+    int index = totalContacts % 8;
+
+    contacts[index].setFirstName(firstName);
+    contacts[index].setLastName(lastName);
+    contacts[index].setNickName(nickName);
+    contacts[index].setPhoneNumber(phoneNumber);
+    contacts[index].setDarkestSecret(darkestSecret);
+
+    totalContacts++;
+
+    std::cout << "Contact added!" << std::endl;
 }
 
 void PhoneBook::searchContact() const
