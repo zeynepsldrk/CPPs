b8b537523d02ddf86af585b43d47fda6007ff447
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-07-16 18:57:36 +0300

    fix: add index member and corresponding getter/setter methods in PhoneBook class

--- cpp00/ex01/includes/PhoneBook.hpp
+++ cpp00/ex01/includes/PhoneBook.hpp
@@ -10,12 +10,15 @@ class PhoneBook
     private:
         Contact contacts[8];
         int totalContacts;
+        int index;
 
     public:
         PhoneBook();
         ~PhoneBook();
         void setTotalContacts(int totalContacts);
         int getTotalContacts() const;
+        void setIndex(int index);
+        int getIndex() const;
         void addContact();
         void searchContact() const;
         int takeField(std::string &field, const std::string output);
