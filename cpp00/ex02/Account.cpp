d6f4378b4ce27f53b6aae5367fedd0e537bfb46a
Zeynep Sila Durak <zedurak@k1m04s01.42istanbul.com.tr>
2026-09-11 20:36:42 +0300

    feat: refactor Account constructor and add timestamp display

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,9 +1,14 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
 
-Account::Account( int initial_deposit ){
-    _amount = initial_deposit;
+Account::Account(int initial_deposit): _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+    _totalAmount += initial_deposit;
     _nbAccounts++;
+
+    _displayTimestamp();
+    std::cout << " index:" <<_accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
 int Account::_nbAccounts = 0;
@@ -13,25 +18,39 @@ int Account::_totalNbWithdrawals = 0;
 
 int	Account::getNbAccounts( void )
 {
-	
+	return _nbAccounts;
 }
 
 int	Account::getTotalAmount( void )
 {
-	
+	return _totalAmount;
 }
 
 int	Account::getNbDeposits( void )
 {
-
+    return _totalNbDeposits;
 }
 
 int	Account::getNbWithdrawals( void )
 {
-
+    return _totalNbWithdrawals;
 }
 
 void	Account::displayAccountsInfos( void )
 {
+    _displayTimestamp();
+    std::cout << " accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits()
+              << ";withdrawals:" << getNbWithdrawals()
+              << std::endl;
+}
+void Account::_displayTimestamp( void )
+{
+    std::time_t now;
+    std::tm *info;
+    char buff[20];
 
+    std::time(&now);
+    info = std::localtime(&now);
+    std::strftime(buff, sizeof(buff), "[%Y%m%d_%H%M%S]", info);
+    std::cout << buff;
 }
