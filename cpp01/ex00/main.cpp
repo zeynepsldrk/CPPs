fe8f5d32e5f31637631cb015b2b7f136402534b1
Zeynep Sila Durak <durakzeynepsila@gmail.com>
2026-09-14 23:24:16 +0300

    feat: enhance main function output for heap zombie creation and deletion

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -4,19 +4,25 @@ int main(void)
 {
     Zombie *heapZombie;
     
-    std::cout << "Heap zombie created." << std::endl;
+    std::cout << "Heap zombie -1- created." << std::endl;
 
     heapZombie = newZombie("A");
     heapZombie->announce();
 
-    std::cout << heapZombie << std::endl;
+    std::cout << "Heap zombie -2- created." << std::endl;
+
+    Zombie *heapZombie2 = newZombie("B");
+    (*heapZombie2).announce();
 
     std::cout << "Stack zombie created." << std::endl;
 
-    Zombie stackZombie("B");
+    Zombie stackZombie("C");
     stackZombie.announce();
 
-    std::cout << "Deleting heap zombie." << std::endl;
+    std::cout << "Deleting heap zombie -1-." << std::endl;
     delete heapZombie;
+    
+    std::cout << "Deleting heap zombie -2-." << std::endl;
+    delete heapZombie2;
 
 }
