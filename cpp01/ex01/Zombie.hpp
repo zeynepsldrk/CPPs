a5cf6f62fc2a6ca0e10491688bdce1b7399c1195
Zeynep Sila Durak <durakzeynepsila@gmail.com>
2026-09-14 23:30:47 +0300

    feat: add initial implementation of Zombie class and related files

---
+++ cpp01/ex01/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    private:
+        std::string name;
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+
+        void announce( void ); //<name>: BraiiiiiiinnnzzzZ...
+        
+};
+
+    Zombie* zombieHorde( int N, std::string name );
+
+#endif
