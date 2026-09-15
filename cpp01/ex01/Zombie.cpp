ef82fb5d81800c2c33cf0fc944e73bb9e145cbb2
Zeynep Sila Durak <zedurak@k1m02s07.42istanbul.com.tr>
2026-09-15 20:02:44 +0300

    feat: implement Zombie class with constructor, destructor, and methods for name setting and announcing

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -1,3 +1,18 @@
 #include "Zombie.hpp"
 
+Zombie::Zombie(){}
 
+Zombie::~Zombie()
+{
+    std::cout << name << " has been destroyed." << std::endl;
+}
+
+void Zombie::setName(std::string name)
+{
+	this->name = name;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
