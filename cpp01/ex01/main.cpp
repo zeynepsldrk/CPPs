e9a816f625a8df1a5e4069668faee96c9edf1099
Zeynep Sila Durak <zedurak@k1m02s07.42istanbul.com.tr>
2026-09-15 20:02:22 +0300

    feat: implement main function to create and announce Zombie horde

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -2,6 +2,18 @@
 
 int main(void)
 {
-    
+	int i = 0;
+    Zombie *horde;
 
+	horde = zombieHorde(5, "Zombie");
+
+	while (i < 5)
+	{
+		horde[i].announce();
+		i++;
+	}
+
+	delete [] horde;
+
+	return 0;
 }
