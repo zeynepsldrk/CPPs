738407d301dcf63e8e758b7ae85ce1dd27641a80
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2026-09-17 11:27:28 +0300

    Update Weapon.cpp

--- cpp01/ex03/src/Weapon.cpp
+++ cpp01/ex03/src/Weapon.cpp
@@ -1,11 +1,6 @@
 #include "Weapon.hpp"
 
-Weapon::Weapon(){}
-
-std::string Weapon::getType()
-{
-	return type;
-}
+Weapon::Weapon(std::string type){}
 
 void Weapon::setType(std::string newType)
 {
